a5582ee471c578edca1fd689c63bb7f1ce4ac1a5
Jardel Matias <jardel.matias@itlean.com.br>
2022-08-01 08:28:06 -0300

    add default error message

--- src/controllers/benefits/get.js
+++ src/controllers/benefits/get.js
@@ -2,12 +2,13 @@ const { validatePermission } = require('../../appServices/authentication')
 const { validateErrorBody } = require('../../appServices/handleError')
 const { validateUserData } = require('./validation')
 const getBenefits = require('../../services/getBenefits')
+const { INTERNAL_SERVER_ERROR } = require('../../helpers/httpConstants')
 
 exports.path = '/benefits'
 exports.method = 'get'
 exports.middleware = [validatePermission, validateUserData, validateErrorBody]
 
-exports.handler = async (req, res, next) => {
+exports.handler = async (req, res) => {
   try {
     const benefits = await getBenefits({
       username: req.body.user,
@@ -21,6 +22,8 @@ exports.handler = async (req, res, next) => {
 
     return res.status(200).json({ beneficios: benefits })
   } catch (err) {
-    next(err)
+    // Log this error in some error logging tool
+    // Sentry could be a good option
+    return res.status(INTERNAL_SERVER_ERROR.status).json(INTERNAL_SERVER_ERROR.response)
   }
 }
